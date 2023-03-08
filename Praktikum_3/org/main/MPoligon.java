825096043e87850575e3b6e2708c413e543626a9
iqbalaliakbar <100354463+iqbalaliakbar@users.noreply.github.com>
2023-03-08 20:43:27 +0800

    Praktikum 3

---
+++ Praktikum_3/org/main/MPoligon.java
@@ -0,0 +1,20 @@
+/**
+* File : MPoligon.java 08/03/2023
+* Penulis : Iqbal Ali Akbar Muhammad
+* Deskripsi : driver class untuk poligon, persegi panjang, dan segitiga
+*/
+package org.main;
+
+import org.bangundatar.*;
+
+public class MPoligon{
+	public static void main(String[] args){
+		PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+		persegi.printInfo();
+		System.out.println("Luas Persegi Panjang : "+persegi.hitungLuas());
+
+		Segitiga segitiga = new Segitiga(5,10,3);
+		segitiga.printInfo();
+		System.out.println("Luas Segitiga : "+segitiga.hitungLuas());
+	}
+}
---
+++ Praktikum_3/org/poligon/Poligon.java
@@ -0,0 +1,17 @@
+/**
+* File : Poligon.java 08/03/2023
+* Penulis : Iqbal Ali Akbar Muhammad
+* Deskripsi : representasi dasar dari objek poligon (segi banyak)
+*/
+
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+
+}
